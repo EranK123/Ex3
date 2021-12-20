fa4f27ca21977c9945603ba34cad3d2a134868cd
EranK123 <93202645+EranK123@users.noreply.github.com>
2021-12-20 16:31:49 +0200

    Delete src directory

--- src/Edge.py
+++
@@ -1,8 +0,0 @@
-class Edge:
-    def __init__(self, src, dest, weight):
-        self.src = src
-        self.dest = dest
-        self.weight = weight
-
-    def setWeight(self, weight):
-        self.weight = weight
